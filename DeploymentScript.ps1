e4c00b070f808054646a6098604e0feec8283cbd
Joshua Irwin <Joshua@irwin.ws>
2016-02-03 22:18:29 -0800

    Hopefully the last bugfix for the initial deployment script

--- DeploymentScript.ps1
+++ DeploymentScript.ps1
@@ -1,24 +1,25 @@
 ﻿$ModConfigFiles = Get-ChildItem -Path .\ModConfigFiles
 $ModSourceFiles = Get-ChildItem -Path .\ModSourceFiles
 $ServerConfigFiles = Get-ChildItem -Path .\ServerConfigFiles
+$BaseServerPath = "C:\RustServerOxide\server\ArkoudaphobiaModded"
 
 Foreach($File in $ModConfigFiles)
 {
     Push-Location .\ModConfigFiles
-    Copy-Item $File -Destination .\oxide\config -Force
+    Copy-Item $File -Destination $BaseServerPath\oxide\config -Force
     Pop-Location
 }
 
 Foreach($File in $ModSourceFiles)
 {
     Push-Location .\ModSourceFiles
-    Copy-Item $File -Destination .\oxide\plugins -Force
+    Copy-Item $File -Destination $BaseServerPath\oxide\plugins -Force
     Pop-Location
 }
 
 Foreach($File in $ServerConfigFiles)
 {
     Push-Location .\ServerConfigFiles
-    Copy-Item $File -Destination .\cfg -Force
+    Copy-Item $File -Destination $BaseServerPath\cfg -Force
     Pop-Location
 }
