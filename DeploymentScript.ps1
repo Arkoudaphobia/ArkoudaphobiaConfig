09a0a85f136307b77c5ab29de5e26a95394b04cd
Joshua Irwin <Joshua@irwin.ws>
2016-02-03 22:01:20 -0800

    Checking in the update script that will be used to deploy updates to rust.

---
+++ DeploymentScript.ps1
@@ -0,0 +1,18 @@
+﻿$ModConfigFiles = Get-ChildItem -Path .\ModConfigFiles
+$ModSourceFiles = Get-ChildItem -Path .\ModSourceFiles
+$ServerConfigFiles = Get-ChildItem -Path .\ServerConfigFiles
+
+Foreach($File in $ModConfigFiles)
+{
+    Copy-Item $File -Destination .\oxide\config -Force
+}
+
+Foreach($File in $ModSourceFiles)
+{
+    Copy-Item $File -Destination .\oxide\plugins -Force
+}
+
+Foreach($File in $ServerConfigFiles)
+{
+    Copy-Item $File -Destination .\cfg -Force
+}
