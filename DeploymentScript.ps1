13f06e809ef13defcf1156ada27441481c2ef748
Joshua Irwin <Joshua@irwin.ws>
2016-02-03 22:05:32 -0800

    Bug fix for the build script

--- DeploymentScript.ps1
+++ DeploymentScript.ps1
@@ -4,15 +4,21 @@ $ServerConfigFiles = Get-ChildItem -Path .\ServerConfigFiles
 
 Foreach($File in $ModConfigFiles)
 {
+    Push-Location .\ModConfigFiles
     Copy-Item $File -Destination .\oxide\config -Force
+    Pop-Location
 }
 
 Foreach($File in $ModSourceFiles)
 {
+    Push-Location .\ModSourceFiles
     Copy-Item $File -Destination .\oxide\plugins -Force
+    Pop-Location
 }
 
 Foreach($File in $ServerConfigFiles)
 {
+    Push-Location .\ServerConfigFiles
     Copy-Item $File -Destination .\cfg -Force
+    Pop-Location
 }
