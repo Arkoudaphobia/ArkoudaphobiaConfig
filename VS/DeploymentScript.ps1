9e84379e168ee073bae34fcb7cb160691ca4a3cd
Joshua Irwin <Joshua@irwin.ws>
2016-02-20 18:44:08 -0800

    *Adding the Reload Request element to the mod element. *Moving the deployment script in to the VS folder. *Starting work on rewriting the deployment script

---
+++ VS/DeploymentScript.ps1
@@ -0,0 +1,20 @@
+﻿$ModConfigFiles = Get-ChildItem -Path .\ModConfigFiles
+$ModSourceFiles = Get-ChildItem -Path .\ModSourceFiles
+$ServerConfigFiles = Get-ChildItem -Path .\ServerConfigFiles
+[XML]$RustManafest = Get-Content .\VS\Manafest.xml
+$BaseServerPath = "C:\RustServerOxide\server\ArkoudaphobiaModded"
+
+Foreach($Mod in $RustManafest.ArkoudaphobiaConfig.ModFiles.Mod)
+{
+	If($Mod.enabled -eq $true)
+	{
+		Copy-Item ($ModConfigFiles + "\" + $Mod.Name) -Destination $BaseServerPath\Oxide\plugins
+	}
+}
+
+Foreach($File in $ServerConfigFiles)
+{
+	Push-Location .\ServerConfigFiles
+	Copy-Item $File -Destination $BaseServerPath\cfg -Force
+	Pop-Location
+}
