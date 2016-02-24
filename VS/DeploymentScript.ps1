f9e3f0b85a95051186882de17d84914f6e84bc81
Joshua Irwin <Joshua@irwin.ws>
2016-02-23 19:02:14 -0800

    Update and logic fix to the deployment script. *Moved the load of the configuration files before the load of the mod files. *Fixed logic issue where a list of files was being used in lue of a string path. *Removed legacy variables from the script so I don't confuse my self again.

--- VS/DeploymentScript.ps1
+++ VS/DeploymentScript.ps1
@@ -1,37 +1,6 @@
-﻿$ModConfigFiles = Get-ChildItem -Path .\ModConfigFiles
-$ModSourceFiles = Get-ChildItem -Path .\ModSourceFiles
-$ServerConfigFiles = Get-ChildItem -Path .\ServerConfigFiles
+﻿$ServerConfigFiles = Get-ChildItem -Path .\ServerConfigFiles
 [XML]$RustManafest = Get-Content .\VS\Manafest.xml
 $BaseServerPath = "C:\RustServerOxide\server\ArkoudaphobiaModded"
-
-Foreach($Mod in $RustManafest.ArkoudaphobiaConfig.ModFiles.Mod)
-{
-	If(($Mod.RequestReload.DateTime -gt (Get-Date).AddHours(-2)) -and ($Mod.RequestReload.DateTime -lt (Get-Date)))
-	{
-		Remove-Item -Path $BaseServerPath\Oxide\plugins\$($Mod.Name)
-		Write-Host "$($Mod.Name) has been unloaded."
-		Start-Sleep -Seconds 5
-	}
-
-	If($Mod.enabled -eq $true)
-	{
-		Copy-Item ($ModSourceFiles + "\" + $Mod.Name) -Destination $BaseServerPath\Oxide\plugins -Force -Confirm:$false
-		Write-Host "$($Mod.Name) uas been loaded / updated."
-	}
-
-	If($Mod.enabled -eq $false)
-	{
-		If((Test-Path -Path $BaseServerPath\Oxide\plugins\$($Mod.Name)) -eq $true)
-		{
-			Remove-Item -Path $BaseServerPath\Oxide\plugins\$($Mod.Name) -Force -Confirm:$false
-			Write-Host "$($Mod.Name) has been removed from the server."
-		}
-		Else
-		{
-			Write-Host "$($Mod.Name) does not exsist on the server.  It can be safely removed from the manafest file now."
-		}
-	}
-}
 
 Foreach($Config in $RustManafest.ArkoudaphobiaConfig.ModConfigFiles.Config)
 {
@@ -49,8 +18,37 @@ Foreach($Config in $RustManafest.ArkoudaphobiaConfig.ModConfigFiles.Config)
 	}
 	If($Config.remove -eq $false)
 	{
-		Copy-Item ($ModConfigFiles + "\" + $Config.Name) -Destination $BaseServerPath\oxide\config -Force -Confirm:$false
+		Copy-Item ".\ModConfigFiles\$($Config.Name)" -Destination $BaseServerPath\oxide\config -Force -Confirm:$false
 		Write-Host "$($Config.Name) has been loaded / updated."
+	}
+}
+
+Foreach($Mod in $RustManafest.ArkoudaphobiaConfig.ModFiles.Mod)
+{
+	If(($Mod.RequestReload.DateTime -gt (Get-Date).AddHours(-2)) -and ($Mod.RequestReload.DateTime -lt (Get-Date)))
+	{
+		Remove-Item -Path $BaseServerPath\Oxide\plugins\$($Mod.Name)
+		Write-Host "$($Mod.Name) has been unloaded."
+		Start-Sleep -Seconds 5
+	}
+
+	If($Mod.enabled -eq $true)
+	{
+		Copy-Item ".\ModSourceFiles\$($Mod.Name)" -Destination $BaseServerPath\Oxide\plugins -Force -Confirm:$false
+		Write-Host "$($Mod.Name) uas been loaded / updated."
+	}
+
+	If($Mod.enabled -eq $false)
+	{
+		If((Test-Path -Path $BaseServerPath\Oxide\plugins\$($Mod.Name)) -eq $true)
+		{
+			Remove-Item -Path $BaseServerPath\Oxide\plugins\$($Mod.Name) -Force -Confirm:$false
+			Write-Host "$($Mod.Name) has been removed from the server."
+		}
+		Else
+		{
+			Write-Host "$($Mod.Name) does not exsist on the server.  It can be safely removed from the manafest file now."
+		}
 	}
 }
 
