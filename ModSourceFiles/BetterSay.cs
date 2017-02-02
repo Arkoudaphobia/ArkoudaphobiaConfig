8b3e62af70bc215b2afeb0a2936ac1b4bc29c05f
Joshua S. Irwin <joshua@irwin.ws>
2017-02-02 14:49:31 -0800

    Updating BetterSay to 2.0.3

--- ModSourceFiles/BetterSay.cs
+++ ModSourceFiles/BetterSay.cs
@@ -5,7 +5,7 @@ using System;
 
 namespace Oxide.Plugins
 {
-    [Info("Better Say", "LaserHydra", "2.0.2", ResourceId = 998)]
+    [Info("Better Say", "LaserHydra", "2.0.3", ResourceId = 998)]
     [Description("Customize the say console command output as you want")]
     class BetterSay : RustPlugin
     {
@@ -46,7 +46,7 @@ namespace Oxide.Plugins
 		
 		object OnServerCommand(ConsoleSystem.Arg arg)
 		{
-			if(arg?.cmd?.namefull != null && arg?.cmd?.namefull == "global.say")
+			if(arg?.cmd?.FullName != null && arg?.cmd?.FullName == "global.say")
 			{
 				if(arg.connection != null && arg.connection.player != null)
 				{
