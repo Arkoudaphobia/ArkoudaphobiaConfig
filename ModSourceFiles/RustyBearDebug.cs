212d9e8895e3849369a659d63afd09380245000c
Joshua S. Irwin <joshua@irwin.ws>
2017-02-03 02:02:30 -0800

    First pass at trying to get my own mod to work for debugging entity data on the server.

---
+++ ModSourceFiles/RustyBearDebug.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Oxide.Core.Plugins;
+
+namespace Oxide.Plugins
+{
+    [Info("RustyBearDebug", "Diablo", "1.0.0")]
+    class RustyBearDebug : RustPlugin
+    {
+        void Loaded()
+        {
+            permission.RegisterPermission("rustybeardebug.use", this);
+        }
+
+        [Command("FindEntity"), Permission("rustybeardebug.use")]
+        void CmdFindEnt(BasePlayer player, string command, string[] args)
+        {
+            if (args[0] == "base")
+            {
+                var entbase = BaseEntity.serverEntities.Where(x => x.ShortPrefabName == args[1]);
+
+                if (entbase == null)
+                {
+                    SendReply(player, "Entity Not FOund");
+                }
+                else
+                {
+                    SendReply(player, $"BaseType  {entbase}");
+                }
+            }
+        }
+    }
+}
