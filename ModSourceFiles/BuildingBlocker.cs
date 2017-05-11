cf35fe2d2fd3d48a3467c31316baa91cafc7b403
Josh Irwin <joshua@irwin.ws>
2017-05-11 12:44:25 -0700

    Adding the building blocker mod to undo what was done in DevBlog 158 w/regards to building twig in a blocked zone.

---
+++ ModSourceFiles/BuildingBlocker.cs
@@ -0,0 +1,177 @@
+using System;
+using System.Collections.Generic;
+using Facepunch;
+using UnityEngine;
+
+namespace Oxide.Plugins
+{
+    [Info("BuildingBlocker", "Vlad-00003", "2.0.3", ResourceId = 2456)]
+    [Description("Blocks building in the building privilage zone. Deactivates raids update.")]
+
+    class BuildingBlocker : RustPlugin
+    {
+		private readonly int triggerLayer = LayerMask.GetMask("Trigger");
+		
+        #region Config setup
+        private string BypassPrivilage = "buildingblocker.bypass";
+        private string Prefix = "[BuildingBlocker]";
+        private string PrefixColor = "#FF3047";
+        #endregion
+
+        #region Vars
+        private Dictionary<string, ItemDefinition> PrefabToItem = new Dictionary<string, ItemDefinition>();
+        #endregion
+
+        #region Localization
+        private string BypassPrivilageCfg = "Bypass block privilage";
+        private string PrefixCfg = "Chat prefix";
+        private string PrefixColorCfg = "Prefix color";
+        private void LoadMessages()
+        {
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                ["Building blocked"] = "Building is blocked."
+            }, this);
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                ["Building blocked"] = "Строительство заблокировано."
+            }, this, "ru");
+        }
+        #endregion
+
+        #region Init
+        protected override void LoadDefaultConfig()
+        {
+            PrintWarning("New configuration file created.");
+        }
+        private void LoadConfigValues()
+        {
+            GetConfig(BypassPrivilageCfg, ref BypassPrivilage);
+            GetConfig(PrefixCfg, ref Prefix);
+            GetConfig(PrefixColorCfg, ref PrefixColor);
+            SaveConfig();
+        }
+        void Loaded()
+        {
+            LoadConfigValues();
+            LoadMessages();
+            permission.RegisterPermission(BypassPrivilage, this);
+
+        }
+        void OnServerInitialized()
+        {
+            foreach(var item in ItemManager.GetItemDefinitions())
+            {
+                var itemdeployable = item?.GetComponent<ItemModDeployable>();
+                if (itemdeployable == null) continue;
+
+                if (!PrefabToItem.ContainsKey(itemdeployable.entityPrefab.resourcePath)) PrefabToItem.Add(itemdeployable.entityPrefab.resourcePath, item);
+            }
+        }
+        #endregion
+
+        #region Main
+        void OnEntityBuilt(Planner planner, GameObject go)
+        {
+            var player = planner.GetOwnerPlayer();
+            if (player == null) return;
+            if (permission.UserHasPermission(player.UserIDString, BypassPrivilage)) return;
+            Vector3 pos = go.transform.position;
+            if (!CanBuildHere(player, pos) && !go.name.Contains("cupboard.tool.deployed"))
+            {
+                SendToChat(player, GetMsg("Building blocked", player.UserIDString));
+                BaseEntity entity = go.GetComponent<BaseEntity>();
+                if (entity == null)
+                {
+                    PrintWarning($"Refund for player {player.displayName} has faild. Entity is null.\nPlease report this error to the plugin thread http://oxidemod.org/threads/building-blocker.25289");
+                    return;
+                }
+                Refund(player, entity);
+                entity.KillMessage();
+                return;
+            }
+        }
+        private bool CanBuildHere(BasePlayer player, Vector3 targetLocation)
+        {
+            var colliders = Pool.GetList<Collider>();
+            var newpos = targetLocation + Vector3.up * 1.09f;
+            Vis.Colliders(newpos, 0.4f, colliders, triggerLayer);
+            foreach (var collider in colliders)
+            {
+                var cup = collider.GetComponentInParent<BuildingPrivlidge>();
+                if (cup == null) continue;
+                
+                if (cup.IsAuthed(player))
+                {
+                    Pool.FreeList(ref colliders);
+                    return true;
+                }else
+                {
+                    Pool.FreeList(ref colliders);
+                    return false;
+                }
+            }
+            Pool.FreeList(ref colliders);
+            return true;
+        }
+        private void Refund(BasePlayer player, BaseEntity entity)
+        {
+            var buildingblock = entity.GetComponent<BuildingBlock>();
+            if (buildingblock != null)
+            {
+                var grade = buildingblock.grade;
+                var blockdef = buildingblock.blockDefinition;
+                if(blockdef == null)
+                {
+                    PrintWarning($"BlockDef for entity {buildingblock.PrefabName} not found and the player {player.displayName} didn't get refund.\nPlease report this error to the plugin thread http://oxidemod.org/threads/building-blocker.25289");
+                    return;
+                }
+                var curGrade = blockdef.grades[(int)grade];
+                foreach (var item in curGrade.costToBuild)
+                {
+                    int amount = (int)item.amount;
+                    if (amount < 1) continue;
+                    player.GiveItem(ItemManager.Create(item.itemDef, amount), BaseEntity.GiveItemReason.Generic);
+                }
+            }
+            else
+            {
+                var itemDef = PrefabToItem[entity.PrefabName];
+                if (itemDef == null)
+                {
+                    PrintWarning($"Item defenition for item {entity.PrefabName} not found and the player {player.displayName} didn't get refund.\nPlease report this error to the plugin thread http://oxidemod.org/threads/building-blocker.25289");
+                    return;
+                }
+                foreach (var item in itemDef.Blueprint.ingredients)
+                {
+                    int amount = (int)item.amount;
+                    if (amount < 1) continue;
+                    player.GiveItem(ItemManager.Create(item.itemDef, amount), BaseEntity.GiveItemReason.Generic);
+                }
+            }
+        }
+        #endregion
+
+        #region Helpers
+        private void SendToChat(BasePlayer Player, string Message)
+        {
+            PrintToChat(Player, "<color=" + PrefixColor + ">" + Prefix + "</color> " + Message);
+        }
+
+        //Перезгрузка функции отправки собщения в чат - отправляет сообщение всем пользователям
+        private void SendToChat(string Message)
+        {
+            PrintToChat("<color=" + PrefixColor + ">" + Prefix + "</color> " + Message);
+        }
+        private void GetConfig<T>(string Key, ref T var)
+        {
+            if (Config[Key] != null)
+            {
+                var = (T)Convert.ChangeType(Config[Key], typeof(T));
+            }
+            Config[Key] = var;
+        }
+        string GetMsg(string key, object userID = null) => lang.GetMessage(key, this, userID == null ? null : userID.ToString());
+        #endregion
+    }
+}
