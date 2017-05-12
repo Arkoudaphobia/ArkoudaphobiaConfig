c508bdd83740cd8efc571b02ae27070ccf12d558
Josh Irwin <joshua@irwin.ws>
2017-05-12 12:28:54 -0700

    Updating BuildingBlocker to 2.1.1

--- ModSourceFiles/BuildingBlocker.cs
+++ ModSourceFiles/BuildingBlocker.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 namespace Oxide.Plugins
 {
-    [Info("BuildingBlocker", "Vlad-00003", "2.0.3", ResourceId = 2456)]
+    [Info("BuildingBlocker", "Vlad-00003", "2.1.1", ResourceId = 2456)]
     [Description("Blocks building in the building privilage zone. Deactivates raids update.")]
 
     class BuildingBlocker : RustPlugin
@@ -16,16 +16,19 @@ namespace Oxide.Plugins
         private string BypassPrivilage = "buildingblocker.bypass";
         private string Prefix = "[BuildingBlocker]";
         private string PrefixColor = "#FF3047";
+        private bool LadderBuilding = false;
         #endregion
 
         #region Vars
-        private Dictionary<string, ItemDefinition> PrefabToItem = new Dictionary<string, ItemDefinition>();
+        //private Dictionary<string, ItemDefinition> PrefabToItem = new Dictionary<string, ItemDefinition>();
+        private static float CupRadius = 1.8f;
         #endregion
 
         #region Localization
         private string BypassPrivilageCfg = "Bypass block privilage";
         private string PrefixCfg = "Chat prefix";
         private string PrefixColorCfg = "Prefix color";
+        private string LadderBuildingCfg = "Allow building ladders in the privilage zone";
         private void LoadMessages()
         {
             lang.RegisterMessages(new Dictionary<string, string>
@@ -49,6 +52,7 @@ namespace Oxide.Plugins
             GetConfig(BypassPrivilageCfg, ref BypassPrivilage);
             GetConfig(PrefixCfg, ref Prefix);
             GetConfig(PrefixColorCfg, ref PrefixColor);
+            GetConfig(LadderBuildingCfg, ref LadderBuilding);
             SaveConfig();
         }
         void Loaded()
@@ -60,42 +64,65 @@ namespace Oxide.Plugins
         }
         void OnServerInitialized()
         {
-            foreach(var item in ItemManager.GetItemDefinitions())
-            {
-                var itemdeployable = item?.GetComponent<ItemModDeployable>();
-                if (itemdeployable == null) continue;
+            //foreach(var item in ItemManager.GetItemDefinitions())
+            //{
+            //    var itemdeployable = item?.GetComponent<ItemModDeployable>();
+            //    if (itemdeployable == null) continue;
 
-                if (!PrefabToItem.ContainsKey(itemdeployable.entityPrefab.resourcePath)) PrefabToItem.Add(itemdeployable.entityPrefab.resourcePath, item);
-            }
+            //    if (!PrefabToItem.ContainsKey(itemdeployable.entityPrefab.resourcePath)) PrefabToItem.Add(itemdeployable.entityPrefab.resourcePath, item);
+            //}
         }
         #endregion
 
         #region Main
-        void OnEntityBuilt(Planner planner, GameObject go)
+        object CanBuild(Planner plan, Construction prefab, Vector3 location)
         {
-            var player = planner.GetOwnerPlayer();
-            if (player == null) return;
-            if (permission.UserHasPermission(player.UserIDString, BypassPrivilage)) return;
-            Vector3 pos = go.transform.position;
-            if (!CanBuildHere(player, pos) && !go.name.Contains("cupboard.tool.deployed"))
+            BasePlayer player = plan.GetOwnerPlayer();
+            if (!player) return null;
+            if (permission.UserHasPermission(player.UserIDString, BypassPrivilage)) return null;
+            if (LadderBuilding && prefab.fullName.Contains("ladder.wooden")) return null;
+
+            Vector3 pos = new Vector3(0, 0, 0);
+            if(location == pos)
+            {
+                pos = player.ServerPosition;
+            }else
+            {
+                pos = location;
+            }
+
+            if (!CanBuildHere(player, pos))
             {
                 SendToChat(player, GetMsg("Building blocked", player.UserIDString));
-                BaseEntity entity = go.GetComponent<BaseEntity>();
-                if (entity == null)
-                {
-                    PrintWarning($"Refund for player {player.displayName} has faild. Entity is null.\nPlease report this error to the plugin thread http://oxidemod.org/threads/building-blocker.25289");
-                    return;
-                }
-                Refund(player, entity);
-                entity.KillMessage();
-                return;
+                return false;
             }
+            return null;
         }
+        //void OnEntityBuilt(Planner planner, GameObject go)
+        //{
+        //    var player = planner.GetOwnerPlayer();
+        //    if (player == null) return;
+        //    if (permission.UserHasPermission(player.UserIDString, BypassPrivilage)) return;
+        //    Vector3 pos = go.transform.position;
+        //    if (!CanBuildHere(player, pos) && !go.name.Contains("cupboard.tool.deployed"))
+        //    {
+        //        SendToChat(player, GetMsg("Building blocked", player.UserIDString));
+        //        BaseEntity entity = go.GetComponent<BaseEntity>();
+        //        if (entity == null)
+        //        {
+        //            PrintWarning($"Refund for player {player.displayName} has faild. Entity is null.\nPlease report this error to the plugin thread http://oxidemod.org/threads/building-blocker.25289");
+        //            return;
+        //        }
+        //        Refund(player, entity);
+        //        entity.KillMessage();
+        //        return;
+        //    }
+        //}
         private bool CanBuildHere(BasePlayer player, Vector3 targetLocation)
         {
             var colliders = Pool.GetList<Collider>();
             var newpos = targetLocation + Vector3.up * 1.09f;
-            Vis.Colliders(newpos, 0.4f, colliders, triggerLayer);
+            Vis.Colliders(newpos, CupRadius, colliders, triggerLayer);
             foreach (var collider in colliders)
             {
                 var cup = collider.GetComponentInParent<BuildingPrivlidge>();
@@ -114,42 +141,42 @@ namespace Oxide.Plugins
             Pool.FreeList(ref colliders);
             return true;
         }
-        private void Refund(BasePlayer player, BaseEntity entity)
-        {
-            var buildingblock = entity.GetComponent<BuildingBlock>();
-            if (buildingblock != null)
-            {
-                var grade = buildingblock.grade;
-                var blockdef = buildingblock.blockDefinition;
-                if(blockdef == null)
-                {
-                    PrintWarning($"BlockDef for entity {buildingblock.PrefabName} not found and the player {player.displayName} didn't get refund.\nPlease report this error to the plugin thread http://oxidemod.org/threads/building-blocker.25289");
-                    return;
-                }
-                var curGrade = blockdef.grades[(int)grade];
-                foreach (var item in curGrade.costToBuild)
-                {
-                    int amount = (int)item.amount;
-                    if (amount < 1) continue;
-                    player.GiveItem(ItemManager.Create(item.itemDef, amount), BaseEntity.GiveItemReason.Generic);
-                }
-            }
-            else
-            {
-                var itemDef = PrefabToItem[entity.PrefabName];
-                if (itemDef == null)
-                {
-                    PrintWarning($"Item defenition for item {entity.PrefabName} not found and the player {player.displayName} didn't get refund.\nPlease report this error to the plugin thread http://oxidemod.org/threads/building-blocker.25289");
-                    return;
-                }
-                foreach (var item in itemDef.Blueprint.ingredients)
-                {
-                    int amount = (int)item.amount;
-                    if (amount < 1) continue;
-                    player.GiveItem(ItemManager.Create(item.itemDef, amount), BaseEntity.GiveItemReason.Generic);
-                }
-            }
-        }
+        //private void Refund(BasePlayer player, BaseEntity entity)
+        //{
+        //    var buildingblock = entity.GetComponent<BuildingBlock>();
+        //    if (buildingblock != null)
+        //    {
+        //        var grade = buildingblock.grade;
+        //        var blockdef = buildingblock.blockDefinition;
+        //        if(blockdef == null)
+        //        {
+        //            PrintWarning($"BlockDef for entity {buildingblock.PrefabName} not found and the player {player.displayName} didn't get refund.\nPlease report this error to the plugin thread http://oxidemod.org/threads/building-blocker.25289");
+        //            return;
+        //        }
+        //        var curGrade = blockdef.grades[(int)grade];
+        //        foreach (var item in curGrade.costToBuild)
+        //        {
+        //            int amount = (int)item.amount;
+        //            if (amount < 1) continue;
+        //            player.GiveItem(ItemManager.Create(item.itemDef, amount), BaseEntity.GiveItemReason.Generic);
+        //        }
+        //    }
+        //    else
+        //    {
+        //        var itemDef = PrefabToItem[entity.PrefabName];
+        //        if (itemDef == null)
+        //        {
+        //            PrintWarning($"Item defenition for item {entity.PrefabName} not found and the player {player.displayName} didn't get refund.\nPlease report this error to the plugin thread http://oxidemod.org/threads/building-blocker.25289");
+        //            return;
+        //        }
+        //        foreach (var item in itemDef.Blueprint.ingredients)
+        //        {
+        //            int amount = (int)item.amount;
+        //            if (amount < 1) continue;
+        //            player.GiveItem(ItemManager.Create(item.itemDef, amount), BaseEntity.GiveItemReason.Generic);
+        //        }
+        //    }
+        //}
         #endregion
 
         #region Helpers
